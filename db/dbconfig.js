e5c7dcabd3a4a13a9f2bb057dd96738e18b282aa
sreeharitblr <sreehari.tblr@gmail.com>
2021-04-28 17:53:23 +0530

    first commit

---
+++ db/dbconfig.js
@@ -0,0 +1,24 @@
+module.exports = {
+
+    HOST: "localhost",
+    
+    USER: "root",
+    
+    PASSWORD: "password",
+    
+    DB: "test",
+    
+    dialect: "mysql",
+    
+    pool: {
+    
+    max: 5,
+    
+    min: 0,
+    
+    acquire: 30000,
+    
+    idle: 10000
+    
+    }
+}    
